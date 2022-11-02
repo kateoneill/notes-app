cd6702797ea3e26f4cbb5cc0ec6a136477f053bc
kateoneill <20088535@mail.wit.ie>
2022-11-02 15:49:36 +0000

    Add main.kt functionality to count all, active and archived notes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -69,8 +69,9 @@ fun mainMenu() : Int {
           > |   3) Update a note             |
           > |   4) Delete a note             |
           > |   5) Archive notes             |
-          > |   6) Save notes                |
-          > |   7) Load notes                |
+          > |   6) Count notes               |
+          > |   10) Save notes               |
+          > |   11) Load notes               |
           > ----------------------------------$reset 
           > |   0) Exit                      |
           > _________________________ooo______
@@ -93,8 +94,9 @@ fun runMenu() {
             3 -> updateNote()
             4 -> deleteNote()
             5 -> archiveNote()
-            6 -> save()
-            7 -> load()
+            6 -> countNotes()
+            10 -> save()
+            11 -> load()
             0 -> exitApp()
             else -> println("Invalid option entered: $option")
         }
@@ -160,6 +162,28 @@ fun listNotes(){
             2 -> listActiveNotes();
             3 -> listArchivedNotes();
             4 -> listNotesBySelectedPriority();
+            else -> println("Invalid option entered: " + option);
+        }
+    } else {
+        println("Option Invalid - There are no notes");
+    }
+}
+
+fun countNotes(){
+    if (noteAPI.numberOfNotes() > 0) {
+        val option = readNextInt(
+            """
+                  > --------------------------------
+                  > |   1) Count ALL notes         |
+                  > |   2) Count ACTIVE notes      |
+                  > |   3) Count ARCHIVED notes    |
+                  > --------------------------------
+         > ==>> """.trimMargin(">"))
+
+        when (option) {
+            1 -> countAllNotes();
+            2 -> countActiveNotes();
+            3 -> countArchivedNotes();
             else -> println("Invalid option entered: " + option);
         }
     } else {
@@ -249,3 +273,14 @@ fun listNotesBySelectedPriority(){
     }
 }
 
+fun countAllNotes() {
+    println(noteAPI.numberOfNotes())
+}
+
+fun countActiveNotes() {
+    println(noteAPI.numberOfActiveNotes())
+}
+
+fun countArchivedNotes() {
+    println(noteAPI.numberOfArchivedNotes())
+}
